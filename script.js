94779b35c0e079809d204e48ea89b2278bdb1c11
Shantanu Rastogi <codershantanu94@gmail.com>
2024-10-03 00:55:48 +0530

    Fixed responsiveness issues
    Fixed responsiveness issues

--- script.js
+++ script.js
@@ -3,22 +3,18 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-//declaring constant to store nav bar icon image element
+//declaring constant and variables
 const menuIcon = document.querySelector("nav .icon img");
-//declaring constant to store all nav bar links
 const navLinks = document.querySelectorAll("#nav-right .box a");
-
-let ImgContainerArry = [];
-//declaring variable to store loader direction for each page
-let loaderDirection = "";
-//declaring variable to store activePage info that will be used in code for dynamic page functions
+const largeImageContainers = document.querySelectorAll(".largeDevice");
+const smallImageContainer = document.querySelector("#smallDevice");
+let loaderDirection = "toBottom";
 let currentPage = window.location.pathname;
-//variable to store window's width
-let windowWidth = window.innerWidth;
-//declaring variable to store device type
-let isLargeDevice = false;
+let imagesArry = [];
+let sortedImageArry = [];
+
 //function controling loader functionality when page is refreshed. A small Video plays and vanishes.
-const loaderHandler = () => {
+function loaderHandler() {
   let tl = gsap.timeline();
   if (loaderDirection == "toBottom") {
     tl.to(".yellowPage1", {
@@ -27,25 +23,17 @@ const loaderHandler = () => {
       duration: 0.5,
       ease: "expo.out",
     })
-      .from(
-        ".yellowPage2",
-        {
-          top: "100%",
-          delay: 0.5,
-          duration: 0.7,
-          ease: "expo.out",
-        },
-        "anim"
-      )
-      .to(
-        ".loader h1",
-        {
-          delay: 0.6,
-          duration: 0.3,
-          color: "black",
-        },
-        "anim"
-      )
+      .from(".yellowPage2", {
+        top: "100%",
+        delay: 0.5,
+        duration: 0.7,
+        ease: "expo.out",
+      })
+      .to(".loader h1", {
+        delay: -0.5,
+        duration: 0.2,
+        color: "black",
+      })
       .to(".loader", {
         delay: -0.8,
         duration: 0.5,
@@ -70,10 +58,9 @@ const loaderHandler = () => {
         display: "none",
       });
   }
-};
-
+}
 //function controling nav bar based on active page and determining loader direction
-const activePageHandler = () => {
+function activePageHandler() {
   navLinks.forEach((link) => {
     //underline active page
     if (link.href.includes(currentPage)) {
@@ -85,7 +72,6 @@ const activePageHandler = () => {
       link.parentElement.classList.remove("active");
     }
   });
-
   //setting logo and nav icon color based on active page and determining loader direction
   let activeElem = document.querySelector(".active");
   activeElem.gete;
@@ -105,11 +91,9 @@ const activePageHandler = () => {
   } else {
     loaderDirection = "toBottom";
   }
-};
-
+}
 //function controlling display of nav menu when nav icon is clicked
-
-const navBarHandler = () => {
+function navBarHandler() {
   let antiClockrotate = true;
   menuIcon.addEventListener("click", () => {
     if (antiClockrotate) {
@@ -118,7 +102,7 @@ const navBarHandler = () => {
         duration: 1,
         ease: "Power1.easeInOut",
       });
-      if (windowWidth < 1024) {
+      if (window.innerWidth < 1024) {
         let smallNavOpenTL = new gsap.timeline();
         smallNavOpenTL
           .to("#nav-right", {
@@ -225,7 +209,7 @@ const navBarHandler = () => {
         duration: 1,
         ease: Power1,
       });
-      if (windowWidth < 1024) {
+      if (window.innerWidth < 1024) {
         let smallNavCloseTL = gsap.timeline();
         smallNavCloseTL
           .to(
@@ -280,7 +264,7 @@ const navBarHandler = () => {
           .to(
             ".active",
             {
-              delay: 0.1,
+              delay: 0.2,
               opacity: 1,
               duration: 0.1,
             },
@@ -289,8 +273,7 @@ const navBarHandler = () => {
           .from(
             ".active",
             {
-              delay: 0.1,
-              x: "290%",
+              x: "285%",
               ease: Power1,
               duration: 1,
             },
@@ -311,106 +294,9 @@ const navBarHandler = () => {
       antiClockrotate = true;
     }
   });
-};
-
-// //function controling endless movement of background text
-const page2Handler = () => {
-  let elems = document.querySelectorAll("#page2 .elem");
-  let page2 = document.querySelector("#page2");
-  let prevElem;
-  let pos = 0;
-  setInterval(() => {
-    if (pos < elems.length) {
-      if (prevElem) {
-        prevElem.querySelector(".moving").classList.toggle("selected");
-        prevElem.querySelector("h1").style.color = "white";
-        prevElem.querySelector("h1").style.fontStyle = "normal";
-      }
-      let currElem = elems[pos];
-      let bgimg = currElem.getAttribute("data-img");
-      page2.style.backgroundImage = `URL(${bgimg})`;
-      pos++;
-      currElem.querySelector("h1").style.color = "black";
-      currElem.querySelector("h1").style.fontStyle = "italic";
-      currElem.querySelector(".moving").classList.toggle("selected");
-      prevElem = currElem;
-    } else {
-      pos = 0;
-    }
-  }, 5000);
-};
-
-// //function controlling scrolling to particular section on a page
-const sectionLinkHandler = () => {
-  let arrowElems = document.querySelectorAll(".arrow");
-  arrowElems.forEach((arrow) => {
-    arrow.addEventListener("click", (evt) => {
-      evt.preventDefault;
-      let scrollToElem;
-      if (currentPage.includes("index.html")) {
-        if (evt.target.parentElement.id == "arrow-contactInfo") {
-          scrollToElem = document.querySelector("#page2");
-        } else if (evt.target.parentElement.id == "arrow-page2Footer") {
-          scrollToElem = document.querySelector("#page3");
-        } else {
-          scrollToElem = document.querySelector("#page1");
-        }
-      } else if (currentPage.includes("about.html")) {
-        if (evt.target.parentElement.id == "arrow-info") {
-          scrollToElem = document.querySelector("#about-page2");
-        } else {
-          scrollToElem = document.querySelector("#about-page1");
-        }
-      }
-      scroll.scrollTo(scrollToElem);
-    });
-  });
-};
-
-// //function to determine order of images based on device
-const page3Handler = () => {
-  const originalImageArry = [];
-  let ImgContainerArry = document.querySelectorAll(".largeDevice");
-
-  ImgContainerArry.forEach((container) => {
-    originalImageArry.push(container.children);
-  });
-  let iterationCount = 0;
-  originalImageArry.forEach((arry) => {
-    let currElemLength = arry.length;
-    if (currElemLength > iterationCount) {
-      iterationCount = currElemLength;
-    }
-  });
-  if (window.innerWidth < 600) {
-    let sortedImageArry = [];
-    ImgContainerArry.forEach((container) => {
-      container.style.display = "none";
-    });
-
-    for (let i = 0; i < iterationCount; i++) {
-      for (let l = 0; l < originalImageArry.length; l++) {
-        if (originalImageArry[l][i]) {
-          sortedImageArry.push(originalImageArry[l][i].cloneNode(true));
-        }
-      }
-    }
-    let smallDeviceContainer = document.getElementById("line4");
-    if (smallDeviceContainer) {
-      smallDeviceContainer.style.display = "flex";
-      sortedImageArry.forEach((image) => {
-        smallDeviceContainer.appendChild(image);
-      });
-    }
-  } else {
-    ImgContainerArry.forEach((container) => {
-      container.style.display = "flex";
-      document.getElementById("line4").style.display = "none";
-    });
-  }
-};
+}
 // //control logo and nav icon color based on active page
-const navIconHandler = () => {
+function navIconHandler() {
   let logo = document.querySelector(".nav-top-bar svg path");
   let anchorElms = document.querySelectorAll("nav a");
   let underlineElems = document.querySelectorAll("nav .underline");
@@ -501,56 +387,54 @@ const navIconHandler = () => {
       },
     });
   }
-};
-
+}
 //function controling nav menu display when nav links are clicked
-// const navLinkclickHandler = () => {
-//   navLinks.forEach((link) => {
-//     link.addEventListener("click", (evt) => {
-//       window.onload = console.log(evt);
-//       let tl = gsap.timeline();
-//       tl.to(menuIcon, {
-//         rotate: "90deg",
-//         duration: 1,
-//         ease: Power1,
-//       })
-//         .to(
-//           "#nav-right .box",
-//           {
-//             delay: -1.2,
-//             opacity: 0,
-//             ease: Power3,
-//             duration: 0.3,
-//             stagger: 0.2,
-//             reversed: true,
-//           },
-//           "hide"
-//         )
-//         .to(
-//           ".active .underline",
-//           {
-//             delay: -1.2,
-//             scaleX: "0",
-//           },
-//           "hide"
-//         )
-//         .to("#navContact", {
-//           opacity: 0,
-//           duration: 0.1,
-//           ease: Power3,
-//           delay: -1.1,
-//         })
-//         .to("#nav-right", {
-//           delay: 0.1,
-//           display: "flex",
-//           transform: "scaleY(0)",
-//           duration: 0.2,
-//           ease: Power3,
-//         });
-//     });
-//   });
-// };
-
+function navLinkclickHandler() {
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (evt) => {
+      window.onload = console.log(evt);
+      let tl = gsap.timeline();
+      tl.to(menuIcon, {
+        rotate: "90deg",
+        duration: 1,
+        ease: Power1,
+      })
+        .to(
+          "#nav-right .box",
+          {
+            delay: -1.2,
+            opacity: 0,
+            ease: Power3,
+            duration: 0.3,
+            stagger: 0.2,
+            reversed: true,
+          },
+          "hide"
+        )
+        .to(
+          ".active .underline",
+          {
+            delay: -1.2,
+            scaleX: "0",
+          },
+          "hide"
+        )
+        .to("#navContact", {
+          opacity: 0,
+          duration: 0.1,
+          ease: Power3,
+          delay: -1.1,
+        })
+        .to("#nav-right", {
+          delay: 0.1,
+          display: "flex",
+          transform: "scaleY(0)",
+          duration: 0.2,
+          ease: Power3,
+        });
+    });
+  });
+}
 //function to reset nav bar on window resize
 function resetNavIcon(isLargeDevice) {
   const navRight = document.querySelector("#nav-right");
@@ -581,28 +465,119 @@ function resetNavIcon(isLargeDevice) {
     document.querySelector(".active").style.position = "relative";
   }
 }
-
-//on screen resize resetting nav bar and calling function;
-const resizeHandler = () => {
-  window.addEventListener("resize", () => {
-    windowWidth = window.innerWidth;
-    if (windowWidth < 1024) {
-      isLargeDevice = false;
-      resetNavIcon(isLargeDevice);
+//function controling endless movement of background text
+function page2Handler() {
+  let elems = document.querySelectorAll("#page2 .elem");
+  let page2 = document.querySelector("#page2");
+  let prevElem;
+  let pos = 0;
+  setInterval(() => {
+    if (pos < elems.length) {
+      if (prevElem) {
+        prevElem.querySelector(".moving").classList.toggle("selected");
+        prevElem.querySelector("h1").style.color = "white";
+        prevElem.querySelector("h1").style.fontStyle = "normal";
+      }
+      let currElem = elems[pos];
+      let bgimg = currElem.getAttribute("data-img");
+      page2.style.backgroundImage = `URL(${bgimg})`;
+      pos++;
+      currElem.querySelector("h1").style.color = "black";
+      currElem.querySelector("h1").style.fontStyle = "italic";
+      currElem.querySelector(".moving").classList.toggle("selected");
+      prevElem = currElem;
     } else {
-      isLargeDevice = true;
-      resetNavIcon(isLargeDevice);
-      page3Handler();
+      pos = 0;
+    }
+  }, 5000);
+}
+//function controlling scrolling to particular section on a page
+function sectionLinkHandler() {
+  let arrowElems = document.querySelectorAll(".arrow");
+  arrowElems.forEach((arrow) => {
+    arrow.addEventListener("click", (evt) => {
+      evt.preventDefault;
+      let scrollToElem;
+      if (currentPage.includes("index.html")) {
+        if (evt.target.parentElement.id == "arrow-contactInfo") {
+          scrollToElem = document.querySelector("#page2");
+        } else if (evt.target.parentElement.id == "arrow-page2Footer") {
+          scrollToElem = document.querySelector("#page3");
+        } else {
+          scrollToElem = document.querySelector("#page1");
+        }
+      } else if (currentPage.includes("about.html")) {
+        if (evt.target.parentElement.id == "arrow-info") {
+          scrollToElem = document.querySelector("#about-page2");
+        } else {
+          scrollToElem = document.querySelector("#about-page1");
+        }
+      }
+      scroll.scrollTo(scrollToElem);
+    });
+  });
+}
+//function to sort images
+function updateSmallDeviceImages() {
+  largeImageContainers.forEach((container) => {
+    imagesArry.push(container.children);
+  });
+  let iterationCount = 0;
+  imagesArry.forEach((arry) => {
+    let currElemLength = arry.length;
+    if (currElemLength > iterationCount) {
+      iterationCount = currElemLength;
     }
   });
-};
-
+  for (let i = 0; i < iterationCount; i++) {
+    for (let l = 0; l < imagesArry.length; l++) {
+      if (imagesArry[l][i]) {
+        sortedImageArry.push(imagesArry[l][i].cloneNode(true));
+      }
+    }
+  }
+  sortedImageArry.forEach((image) => {
+    smallImageContainer.appendChild(image);
+  });
+}
+//function to display images according to large or small device
+function page3Handler() {
+  let smallDeviceContainer = document.querySelector(
+    ".smallDevice-IMGcontainer"
+  );
+  let largeDeviceContainer = document.querySelector(
+    ".largeDevice-IMGcontainer"
+  );
+  if (smallDeviceContainer || largeDeviceContainer) {
+    if (window.innerWidth < 700) {
+      smallDeviceContainer.style.display = "flex";
+      largeDeviceContainer.style.display = "none";
+    } else {
+      largeDeviceContainer.style.display = "flex";
+      smallDeviceContainer.style.display = "none";
+    }
+  }
+}
+//on screen resize resetting nav bar and page3 images layout
+function resizeHandler() {
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 1024) {
+      resetNavIcon(false);
+    } else {
+      resetNavIcon(true);
+    }
+    page3Handler();
+  });
+}
 //invoking functions
-resizeHandler();
-activePageHandler();
-loaderHandler();
-sectionLinkHandler();
-navBarHandler();
-page2Handler();
-page3Handler();
-navIconHandler();
+window.addEventListener("load", () => {
+  updateSmallDeviceImages();
+  navIconHandler();
+  activePageHandler();
+  loaderHandler();
+  sectionLinkHandler();
+  navBarHandler();
+  page2Handler();
+  page3Handler();
+  resizeHandler();
+});
